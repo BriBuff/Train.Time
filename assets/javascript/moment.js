1250916f5a77c719fd1ec16233b0fb98c9c4c127
Bri Buffington <bri.kudos@gmail.com>
2018-11-18 20:53:14 -0700

    setup

---
+++ assets/javascript/moment.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCKKzb_QwQJpw8BS3ZlUHQUmHNmQTg88uA",
+    authDomain: "train-c4203.firebaseapp.com",
+    databaseURL: "https://train-c4203.firebaseio.com",
+    projectId: "train-c4203",
+    storageBucket: "train-c4203.appspot.com",
+    messagingSenderId: "444381081958"
+  };
+  firebase.initializeApp(config);
